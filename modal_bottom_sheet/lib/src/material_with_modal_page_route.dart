059f67782989f60f334bd5a1edd98cb5ebf2eb5e
Bayu Saputra <babahayoo@gmail.com>
2023-02-02 14:55:23 +0700

    hide ModalBottomSheet route on every import material

--- modal_bottom_sheet/lib/src/material_with_modal_page_route.dart
+++ modal_bottom_sheet/lib/src/material_with_modal_page_route.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide ModalBottomSheetRoute;
 
 import 'bottom_sheet_route.dart';
 
